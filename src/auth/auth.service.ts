74d68ee0ca87e03e0c56f46195115a29eec8ad9b
Arco Estrategias <arcoestrategias@outlook.com>
2024-11-29 17:28:24 -0500

    Configuracion inicial d JWT

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,75 @@
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import * as bcrypt from 'bcrypt';
+
+import { User } from './entities/user.entity';
+import { CreateUserDto, LoginUserDto } from './dto';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+
+    private readonly jwtService: JwtService
+  ){}
+  
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const { password, ...userData } = createUserDto;
+
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync( password, 10 )
+      })
+
+      await this.userRepository.save( user )
+
+      return {
+        ...user,
+        token: this.getJwtToken({ email: user.email })
+      };
+    } catch (error) {
+      this.handleDBErrors(error);
+    }
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+      const { password, email } = loginUserDto;
+
+      const user = await this.userRepository.findOne({
+        where: { email },
+        select: { email: true, password: true}
+      });
+
+      if( !user )
+        throw new UnauthorizedException(`Credentials are not valid (email)`);
+
+      if( !bcrypt.compareSync( password, user.password ) )
+        throw new UnauthorizedException(`Credentials are not valid (password)`);
+
+      return {
+        ...user,
+        token: this.getJwtToken({ email: user.email })
+      };
+  }
+
+  private getJwtToken( payload: JwtPayload) {
+    const token = this.jwtService.sign( payload );
+    return token;
+  }
+
+  private handleDBErrors( error: any ): never {
+    if( error.code === '23505')
+      throw new BadRequestException( error.detail );
+
+    console.log(error);
+
+    throw new InternalServerErrorException('Please check server logs')
+    
+  }
+}
